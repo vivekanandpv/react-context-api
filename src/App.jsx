248a8ccffd03395ee61ffd3ca94a548a756a5088
vivekanandpv <vivekanandpv@outlook.com>
2021-06-18 19:47:08 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <div className='container p5'>
+        <h3>Learning Context API</h3>
+        <hr />
+      </div>
+    </>
+  );
+}
+
+export default App;
