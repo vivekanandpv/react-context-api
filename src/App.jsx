cf62f3c377ace902b021ce4071a0b4b41e71e1cc
vivekanandpv <vivekanandpv@outlook.com>
2021-06-18 19:57:02 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
 function App() {
+  //  Creating context
+  const AppTheme = React.createContext('light');
+
   return (
     <>
       <div className='container p-5'>
         <h3>Learning Context API</h3>
         <hr />
+        <AppTheme.Provider value='static context prop'></AppTheme.Provider>
       </div>
     </>
   );
